0e879b355b50ef8489063aa5e3d109cd340b7084
nassimyt312-debug <nassimyt312@gmail.com>
2026-08-19 17:10:27 +0100

    Update SSLAnalyzer.swift

--- SSLAnalyzer.swift
+++ SSLAnalyzer.swift
@@ -1,30 +1,19 @@
 import Foundation
-import Security
 
 class SSLAnalyzer {
     static func analyze(url: URL, completion: @escaping (String) -> Void) {
-        let session = URLSession(configuration: .default, delegate: SSLDelegate(completion: completion), delegateQueue: nil)
-        let task = session.dataTask(with: url)
-        task.resume()
-    }
-}
-
-class SSLDelegate: NSObject, URLSessionDataDelegate {
-    let completion: (String) -> Void
-    
-    init(completion: @escaping (String) -> Void) {
-        self.completion = completion
-    }
-    
-    func urlSession(_ session: URLSession, didReceive challenge: URLSessionChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
-           let trust = challenge.protectionSpace.serverTrust {
-            var error: CFError?
-            let isTrusted = SecTrustEvaluateWithError(trust, &error)
-            completion(isTrusted ? "SSL Certificate Valid" : "SSL Certificate Untrusted")
-        } else {
-            completion("No SSL Data Available")
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { _, response, error in
+            if let error = error {
+                completion("[✕] SSL Error: \(error.localizedDescription)")
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                completion("[✓] SSL Connection Successful (Status: \(httpResponse.statusCode))")
+            } else {
+                completion("[✓] SSL Connection Established")
+            }
         }
-        completionHandler(.performDefaultHandling, nil)
+        task.resume()
     }
 }
