e56d13fa399b79936f0c3e3fa1ad3b4864aa0111
nassimyt312-debug <nassimyt312@gmail.com>
2026-08-19 17:03:31 +0100

    Update SSLAnalyzer.swift

--- SSLAnalyzer.swift
+++ SSLAnalyzer.swift
@@ -3,24 +3,20 @@ import Security
 
 class SSLAnalyzer {
     static func analyze(url: URL, completion: @escaping (String) -> Void) {
-        guard let host = url.host else {
-            completion("Invalid Host")
-            return
-        }
         let session = URLSession(configuration: .default, delegate: SSLDelegate(completion: completion), delegateQueue: nil)
         let task = session.dataTask(with: url)
         task.resume()
     }
 }
 
-class SSLDelegate: NSObject, URLSessionDelegate {
+class SSLDelegate: NSObject, URLSessionDataDelegate {
     let completion: (String) -> Void
     
     init(completion: @escaping (String) -> Void) {
         self.completion = completion
     }
     
-    func urlSession(_ session: URLSession, challenge: URLSessionChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+    func urlSession(_ session: URLSession, didReceive challenge: URLSessionChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
            let trust = challenge.protectionSpace.serverTrust {
             var error: CFError?
